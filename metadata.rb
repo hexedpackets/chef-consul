2aa632789f282bd66c606298040d407d4d83a508
William Huba <hexedpackets@gmail.com>
2015-08-13 17:44:25 -0400

    Remove source_url for backwards compatability

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,5 @@ license          'Apache 2.0'
 description      'Installs/Configures consul'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
-source_url       'https://github.com/hexedpackets/chef-consul'
 
 depends "poise-service"
